ca4416eb753cf80db88860bd5696029b6a33af18
Marco Di Summa <mdisumma@yahoo.com>
2024-07-08 15:23:29 +0100

    min

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const euro = 1.18;
+// MARCO CODE
 
-const value = prompt();
+// const euro = 1.18;
 
-function calculate() {
-  return value * euro;
+// const value = prompt();
+
+// function calculate() {
+//   return value * euro;
+// }
+
+// console.log(calculate());
+
+// LUISDE CODE
+// let conversion = 0;
+// const dolar_price = 1.28;
+// const euro_price = 1.18;
+
+// function convertGBPtoEUR(amountInPounds) {
+//   conversion = amountInPounds * euro_price;
+//   return conversion;
+// }
+
+// convertGBPtoEUR(2);
+// console.log(conversion);
+
+function add(a, b) {
+  return a + b;
+}
+function minus(a, b) {
+  return a - b;
 }
 
-console.log(calculate());
+// console.log(add(2, 2));
+// console.log(minus(2, 2));
+
+function doSomething(num1, num2) {
+  add(num1, num2);
+}
+console.log(doSomething(5, 2));
